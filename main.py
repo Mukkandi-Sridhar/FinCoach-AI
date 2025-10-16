95962d379c93c2a070a88c0fe841ca988a927145
MUKKANDI_SRIDHAR <sridharraoyalls@gmail.com>
2025-10-16 14:15:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -641,4 +641,5 @@ def chat_route():
     return jsonify({"text": out_text})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
